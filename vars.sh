47dac91d5746c45d7015cabf73cb6e3aea0dd6b2
Filipe Calasans <filipe.calasans@gmail.com>
2021-11-28 21:24:08 -0800

    Refactor bash scripts for reusable variables. Also, allow selecting between gdb-multiarch and arm-none-eabi-gdb.

---
+++ vars.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Define importnat paths and variables commonly used 
+# on other scripts.
+
+THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+export TOOLS_DIR="${THIS_DIR}/tools"
+export NINJA_PATH="${TOOLS_DIR}/ninja/ninja"
+export GCC_TOOLCHAIN_PATH="${TOOLS_DIR}/gcc-arm-none-eabi-10.3"
+export BUILD_OUTPUT="${THIS_DIR}/out"
