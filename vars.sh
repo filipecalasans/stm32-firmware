0c9e88fc9c6f6cb308504b38d2b4192d33129087
Filipe Calasans <filipe.calasans@gmail.com>
2026-01-01 20:14:55 -0800

    Make build hermetic by commiting the prebuilt tools

--- vars.sh
+++ vars.sh
@@ -12,4 +12,4 @@ export ARCHIVES_DIR="${THIS_DIR}/archives"
 export BUILD_OUTPUT="${THIS_DIR}/out"
 
 export NINJA_PATH="${TOOLS_DIR}/ninja/ninja"
-export GCC_TOOLCHAIN_PATH="${TOOLS_DIR}/gcc-arm-none-eabi-10.3"
+export GCC_TOOLCHAIN_PATH="${TOOLS_DIR}/arm-gnu-toolchain-15.2.rel1-x86_64-arm-none-eabi"
