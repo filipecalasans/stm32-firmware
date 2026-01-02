7dfda0bbbe21279c2193bb92181e492941424c48
Filipe Calasans <filipe.calasans@gmail.com>
2026-01-01 21:39:43 -0800

    Pass cmake_dir explicitly as part of the cmake invocation

--- build.sh
+++ build.sh
@@ -12,9 +12,10 @@ source "${THIS_DIR}/vars.sh"
 CMAKE_DIR="${THIS_DIR}/cmake"
 mkdir -p "${BUILD_OUTPUT}"
 
-(cd out/ && cmake ../ -G Ninja \
+(cd ${THIS_DIR}/out/ && cmake ../ -G Ninja \
     -DARM_TOOLCHAIN_PATH="${GCC_TOOLCHAIN_PATH}" \
     -DARM_TARGET_TRIPLET="arm-none-eabi" \
+    -DCMAKE_MODULE_PATH="${CMAKE_DIR}" \
     -DCMAKE_TOOLCHAIN_FILE="${CMAKE_DIR}/toolchain/gcc.cmake" \
     -DCMAKE_MAKE_PROGRAM="${NINJA_PATH}" \
     -DCMAKE_BUILD_TYPE="Debug" \
